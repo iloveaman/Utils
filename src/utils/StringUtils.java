4ec376582a8a22d989564d501407d8a513474541
iloveaman <iloveaman@163.com>
2015-08-28 18:24:37 +0800

    ArrayUtil.java/ObjectUtil.java/StringUtils.java/FileUtils.java

---
+++ src/utils/StringUtils.java
@@ -0,0 +1,362 @@
+package com.kocla.preparationtools.utils;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * String Utils
+ * 
+ * @author <a href="http://www.trinea.cn" target="_blank">Trinea</a> 2011-7-22
+ */
+public class StringUtils {
+	
+	public static boolean isEmojiCharacter(char codePoint) {
+        return !((codePoint == 0x0) ||
+                (codePoint == 0x9) ||
+                (codePoint == 0xA) ||
+                (codePoint == 0xD) ||
+                ((codePoint >= 0x20) && (codePoint <= 0xD7FF)) ||
+                ((codePoint >= 0xE000) && (codePoint <= 0xFFFD)) ||
+                ((codePoint >= 0x10000) && (codePoint <= 0x10FFFF)));
+    }
+	/**
+	 * 判断是否多个空格
+	 * @author Aman
+	 * @time 2014年5月20日 10:33:42
+	 */
+	public static boolean isSpace(String src){
+		String regEx0 = "[ ]+"; 
+		Pattern pattern0 = Pattern.compile(regEx0);  
+		Matcher matcher0 = pattern0.matcher(src);  
+		if(matcher0.matches()){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	/**
+	 * 判断是否数字和字母(可以匹配 数字，大小写字母 个数是至少一个)
+	 * @author Aman
+	 * @time 2014年5月20日 10:33:42
+	 */
+	public static boolean isDigitOrAlphabet(String src){
+		String regEx0 = "[\\da-zA-Z]+"; 
+		Pattern pattern0 = Pattern.compile(regEx0);  
+		Matcher matcher0 = pattern0.matcher(src);  
+		if(matcher0.matches()){
+			return true;
+		}else{
+			return false;
+		}
+	}
+    /**
+     * is null or its length is 0 or it is made by space
+     * 
+     * <pre>
+     * isBlank(null) = true;
+     * isBlank(&quot;&quot;) = true;
+     * isBlank(&quot;  &quot;) = true;
+     * isBlank(&quot;a&quot;) = false;
+     * isBlank(&quot;a &quot;) = false;
+     * isBlank(&quot; a&quot;) = false;
+     * isBlank(&quot;a b&quot;) = false;
+     * </pre>
+     * 
+     * @param str
+     * @return if string is null or its size is 0 or it is made by space, return true, else return false.
+     */
+    public static boolean isBlank(String str) {
+        return (str == null || str.trim().length() == 0);
+    }
+
+    /**
+     * is null or its length is 0
+     * 
+     * <pre>
+     * isEmpty(null) = true;
+     * isEmpty(&quot;&quot;) = true;
+     * isEmpty(&quot;  &quot;) = false;
+     * </pre>
+     * 
+     * @param str
+     * @return if string is null or its size is 0, return true, else return false.
+     */
+    public static boolean isEmpty(String str) {
+        return (str == null || str.length() == 0);
+    }
+
+    /**
+     * compare two string
+     * 
+     * @param actual
+     * @param expected
+     * @return
+     * @see ObjectUtils#isEquals(Object, Object)
+     */
+    public static boolean isEquals(String actual, String expected) {
+        return ObjectUtils.isEquals(actual, expected);
+    }
+
+    /**
+     * null string to empty string
+     * 
+     * <pre>
+     * nullStrToEmpty(null) = &quot;&quot;;
+     * nullStrToEmpty(&quot;&quot;) = &quot;&quot;;
+     * nullStrToEmpty(&quot;aa&quot;) = &quot;aa&quot;;
+     * </pre>
+     * 
+     * @param str
+     * @return
+     */
+    public static String nullStrToEmpty(String str) {
+        return (str == null ? "" : str);
+    }
+
+    /**
+     * capitalize first letter
+     * 
+     * <pre>
+     * capitalizeFirstLetter(null)     =   null;
+     * capitalizeFirstLetter("")       =   "";
+     * capitalizeFirstLetter("2ab")    =   "2ab"
+     * capitalizeFirstLetter("a")      =   "A"
+     * capitalizeFirstLetter("ab")     =   "Ab"
+     * capitalizeFirstLetter("Abc")    =   "Abc"
+     * </pre>
+     * 
+     * @param str
+     * @return
+     */
+    public static String capitalizeFirstLetter(String str) {
+        if (isEmpty(str)) {
+            return str;
+        }
+
+        char c = str.charAt(0);
+        return (!Character.isLetter(c) || Character.isUpperCase(c)) ? str : new StringBuilder(str.length())
+                .append(Character.toUpperCase(c)).append(str.substring(1)).toString();
+    }
+
+    /**
+     * encoded in utf-8
+     * 
+     * <pre>
+     * utf8Encode(null)        =   null
+     * utf8Encode("")          =   "";
+     * utf8Encode("aa")        =   "aa";
+     * utf8Encode("啊啊啊啊")   = "%E5%95%8A%E5%95%8A%E5%95%8A%E5%95%8A";
+     * </pre>
+     * 
+     * @param str
+     * @return
+     * @throws UnsupportedEncodingException if an error occurs
+     */
+    public static String utf8Encode(String str) {
+        if (!isEmpty(str) && str.getBytes().length != str.length()) {
+            try {
+                return URLEncoder.encode(str, "UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                throw new RuntimeException("UnsupportedEncodingException occurred. ", e);
+            }
+        }
+        return str;
+    }
+
+    /**
+     * encoded in utf-8, if exception, return defultReturn
+     * 
+     * @param str
+     * @param defultReturn
+     * @return
+     */
+    public static String utf8Encode(String str, String defultReturn) {
+        if (!isEmpty(str) && str.getBytes().length != str.length()) {
+            try {
+                return URLEncoder.encode(str, "UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                return defultReturn;
+            }
+        }
+        return str;
+    }
+
+    /**
+     * get innerHtml from href
+     * 
+     * <pre>
+     * getHrefInnerHtml(null)                                  = ""
+     * getHrefInnerHtml("")                                    = ""
+     * getHrefInnerHtml("mp3")                                 = "mp3";
+     * getHrefInnerHtml("&lt;a innerHtml&lt;/a&gt;")                    = "&lt;a innerHtml&lt;/a&gt;";
+     * getHrefInnerHtml("&lt;a&gt;innerHtml&lt;/a&gt;")                    = "innerHtml";
+     * getHrefInnerHtml("&lt;a&lt;a&gt;innerHtml&lt;/a&gt;")                    = "innerHtml";
+     * getHrefInnerHtml("&lt;a href="baidu.com"&gt;innerHtml&lt;/a&gt;")               = "innerHtml";
+     * getHrefInnerHtml("&lt;a href="baidu.com" title="baidu"&gt;innerHtml&lt;/a&gt;") = "innerHtml";
+     * getHrefInnerHtml("   &lt;a&gt;innerHtml&lt;/a&gt;  ")                           = "innerHtml";
+     * getHrefInnerHtml("&lt;a&gt;innerHtml&lt;/a&gt;&lt;/a&gt;")                      = "innerHtml";
+     * getHrefInnerHtml("jack&lt;a&gt;innerHtml&lt;/a&gt;&lt;/a&gt;")                  = "innerHtml";
+     * getHrefInnerHtml("&lt;a&gt;innerHtml1&lt;/a&gt;&lt;a&gt;innerHtml2&lt;/a&gt;")        = "innerHtml2";
+     * </pre>
+     * 
+     * @param href
+     * @return <ul>
+     *         <li>if href is null, return ""</li>
+     *         <li>if not match regx, return source</li>
+     *         <li>return the last string that match regx</li>
+     *         </ul>
+     */
+    public static String getHrefInnerHtml(String href) {
+        if (isEmpty(href)) {
+            return "";
+        }
+
+        String hrefReg = ".*<[\\s]*a[\\s]*.*>(.+?)<[\\s]*/a[\\s]*>.*";
+        Pattern hrefPattern = Pattern.compile(hrefReg, Pattern.CASE_INSENSITIVE);
+        Matcher hrefMatcher = hrefPattern.matcher(href);
+        if (hrefMatcher.matches()) {
+            return hrefMatcher.group(1);
+        }
+        return href;
+    }
+
+/**
+     * process special char in html
+     * 
+     * <pre>
+     * htmlEscapeCharsToString(null) = null;
+     * htmlEscapeCharsToString("") = "";
+     * htmlEscapeCharsToString("mp3") = "mp3";
+     * htmlEscapeCharsToString("mp3&lt;") = "mp3<";
+     * htmlEscapeCharsToString("mp3&gt;") = "mp3\>";
+     * htmlEscapeCharsToString("mp3&amp;mp4") = "mp3&mp4";
+     * htmlEscapeCharsToString("mp3&quot;mp4") = "mp3\"mp4";
+     * htmlEscapeCharsToString("mp3&lt;&gt;&amp;&quot;mp4") = "mp3\<\>&\"mp4";
+     * </pre>
+     * 
+     * @param source
+     * @return
+     */
+    public static String htmlEscapeCharsToString(String source) {
+        return StringUtils.isEmpty(source) ? source : source.replaceAll("&lt;", "<").replaceAll("&gt;", ">")
+                .replaceAll("&amp;", "&").replaceAll("&quot;", "\"");
+    }
+
+    /**
+     * transform half width char to full width char
+     * 
+     * <pre>
+     * fullWidthToHalfWidth(null) = null;
+     * fullWidthToHalfWidth("") = "";
+     * fullWidthToHalfWidth(new String(new char[] {12288})) = " ";
+     * fullWidthToHalfWidth("！＂＃＄％＆) = "!\"#$%&";
+     * </pre>
+     * 
+     * @param s
+     * @return
+     */
+    public static String fullWidthToHalfWidth(String s) {
+        if (isEmpty(s)) {
+            return s;
+        }
+
+        char[] source = s.toCharArray();
+        for (int i = 0; i < source.length; i++) {
+            if (source[i] == 12288) {
+                source[i] = ' ';
+                // } else if (source[i] == 12290) {
+                // source[i] = '.';
+            } else if (source[i] >= 65281 && source[i] <= 65374) {
+                source[i] = (char)(source[i] - 65248);
+            } else {
+                source[i] = source[i];
+            }
+        }
+        return new String(source);
+    }
+
+    /**
+     * transform full width char to half width char
+     * 
+     * <pre>
+     * halfWidthToFullWidth(null) = null;
+     * halfWidthToFullWidth("") = "";
+     * halfWidthToFullWidth(" ") = new String(new char[] {12288});
+     * halfWidthToFullWidth("!\"#$%&) = "！＂＃＄％＆";
+     * </pre>
+     * 
+     * @param s
+     * @return
+     */
+    public static String halfWidthToFullWidth(String s) {
+        if (isEmpty(s)) {
+            return s;
+        }
+
+        char[] source = s.toCharArray();
+        for (int i = 0; i < source.length; i++) {
+            if (source[i] == ' ') {
+                source[i] = (char)12288;
+                // } else if (source[i] == '.') {
+                // source[i] = (char)12290;
+            } else if (source[i] >= 33 && source[i] <= 126) {
+                source[i] = (char)(source[i] + 65248);
+            } else {
+                source[i] = source[i];
+            }
+        }
+        return new String(source);
+    }
+    /**
+     * 过滤HTML标签，取出文本内容
+     * 
+     * @param inputString HTML字符串
+     * @return 过滤了HTML标签的字符串
+     */
+    public static String filterHtmlTag(String inputString) {
+        if (inputString == null) {
+            return inputString;
+        }
+        
+        String htmlStr = inputString;
+        String textStr = "";
+        Pattern pScript;
+        Matcher mScript;
+        Pattern pStyle;
+        Matcher mStyle;
+        Pattern pHtml;
+        Matcher mHtml;
+        
+        try {
+            // 定义script的正则表达式
+            String regExScript = "<[\\s]*?script[^>]*?>[\\s\\S]*?<[\\s]*?/[\\s]*?script[\\s]*?>";
+            // 定义style的正则表达式
+            String regExStyle = "<[\\s]*?style[^>]*?>[\\s\\S]*?<[\\s]*?/[\\s]*?style[\\s]*?>";
+            // 定义HTML标签的正则表达式
+            String regExHtml = "<[^>\"]+>";
+            
+            pScript = Pattern.compile(regExScript, Pattern.CASE_INSENSITIVE);
+            mScript = pScript.matcher(htmlStr);
+            // 过滤script标签
+            htmlStr = mScript.replaceAll("");
+            
+            pStyle = Pattern.compile(regExStyle, Pattern.CASE_INSENSITIVE);
+            mStyle = pStyle.matcher(htmlStr);
+            // 过滤style标签
+            htmlStr = mStyle.replaceAll("");
+            
+            pHtml = Pattern.compile(regExHtml, Pattern.CASE_INSENSITIVE);
+            mHtml = pHtml.matcher(htmlStr);
+            // 过滤html标签
+            htmlStr = mHtml.replaceAll("");
+            
+            textStr = htmlStr;            
+        } catch (Exception e) {
+            System.err.println("Html2Text: " + e.getMessage());
+        }
+        
+        return textStr;
+    }
+}
