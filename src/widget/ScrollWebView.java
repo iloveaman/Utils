9f7dc1c031855be306b7a8787211edf16f0f730d
iloveaman <iloveaman@163.com>
2016-02-01 18:52:24 +0800

    Update ScrollWebView.java
    带监听滑动位置的webivew,注释部分为供参考的配置

--- src/widget/ScrollWebView.java
+++ src/widget/ScrollWebView.java
@@ -111,3 +111,100 @@ public class ScrollWebView extends WebView {
         }
     }
 }
+// /**
+//      * 电视设置
+//      */
+//     private void tvSetting() {
+//         //自适应任意大小的pc网页
+//         WebSettings settings = webView.getSettings();
+//         //settings.setTextSize(WebSettings.TextSize.NORMAL);
+//         //得到文章字体的大小
+//         int fontSize = 100;
+//         settings.setTextZoom(fontSize);
+//         //
+//         //-------------------------------------------------
+//         if (Build.VERSION.SDK_INT >= 8) {
+//             settings.setPluginState(WebSettings.PluginState.ON);
+//         } else {
+//             //settings.setPluginsEnabled(true);
+//         }
+//         //
+//         webView.setInitialScale(100);
+//         //webView.getSettings().setRenderPriority(WebSettings.RenderPriority.HIGH);
+//         //webView.getSettings().setBlockNetworkImage(true);
+//         // 设置可以支持缩放
+//         settings.setSupportZoom(true);
+//         // 设置出现缩放工具
+//         settings.setBuiltInZoomControls(true);
+//         //setZoomControlGone(webView);
+//         //setZoomControlGone(webView);
+//         webView.hideZoomController();
+//         //扩大比例的缩放
+//         settings.setUseWideViewPort(true);
+//         //自适应屏幕
+//         settings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+//         settings.setLoadWithOverviewMode(true);
+//         //
+//         webView.setVerticalScrollbarOverlay(false);
+//         webView.setHorizontalScrollbarOverlay(false);
+//         //去掉滚动条
+//         webView.setVerticalScrollBarEnabled(false);
+//         webView.setHorizontalScrollBarEnabled(false);
+//         //
+//         //-------------------------------------------------
+//         //Java和Javascript安全交互
+//         settings.setJavaScriptEnabled(true);// 支持js
+//         webView.addJavascriptInterface(new JavaScriptInterface(), "androidlistner");
+//         // 如果页面中链接，如果希望点击链接继续在当前browser中响应，而不是新开Android的系统browser中响应该链接，必须覆盖webview的WebViewClient对象
+//         webView.setWebViewClient(new MyWebViewClient());
+//         webView.setWebChromeClient(new WebChromeClient());
+//         webView.requestFocus(View.FOCUS_DOWN | View.FOCUS_UP);
+//         settings.setLightTouchEnabled(true);
+//         webView.setOnScrollChangeListener(this);
+//     }
+
+//     /**
+//      * 手机设置
+//      */
+//     private void phoneSetting() {
+//         //自适应任意大小的pc网页
+//         WebSettings settings = webView.getSettings();
+//         settings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+//         settings.setUseWideViewPort(false);//;设置此属性，可任意比例缩放。
+//         settings.setUseWideViewPort(false);
+//         settings.setLoadWithOverviewMode(true);
+//         if (Build.VERSION.SDK_INT >= 8) {
+//             settings.setPluginState(WebSettings.PluginState.ON);
+//         } else {
+//             //settings.setPluginsEnabled(true);
+//         }
+//         //去掉滚动条
+//         webView.setVerticalScrollBarEnabled(false);
+//         webView.setHorizontalScrollBarEnabled(false);
+//         int fontSize = 100;
+//         settings.setTextZoom(fontSize);
+//         webView.setInitialScale(100);
+//         ZoomButtonsController zoom_controll;
+//         //去掉缩放按钮
+//         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+//             // Use the API 11+ calls to disable the controls
+//             settings.setBuiltInZoomControls(false);
+//             settings.setDisplayZoomControls(false);
+//         } else {
+//             try {
+//                 Class webview = Class.forName("android.webkit.WebView");
+//                 Method method = webview.getMethod("getZoomButtonsController");
+//                 zoom_controll = (ZoomButtonsController) method.invoke(this, true);
+//                 zoom_controll.setZoomInEnabled(false);
+//                 zoom_controll.setZoomOutEnabled(false);
+//             } catch (Exception e) {
+//                 e.printStackTrace();
+//             }
+//         }
+//         //Java和Javascript安全交互
+//         settings.setJavaScriptEnabled(true);// 支持js
+//         webView.addJavascriptInterface(new JavaScriptInterface(), "androidlistner");
+//         // 如果页面中链接，如果希望点击链接继续在当前browser中响应，而不是新开Android的系统browser中响应该链接，必须覆盖webview的WebViewClient对象
+//         webView.setWebViewClient(new MyWebViewClient());
+//         webView.setWebChromeClient(new WebChromeClient());
+//     }
