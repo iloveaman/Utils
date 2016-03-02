f1f98c8d1568d48b5bb85d48a6a9f269b5c3560b
iloveaman <iloveaman@163.com>
2016-03-02 16:01:00 +0800

    Create RecycleAdapter.java
    RecycleAdapter demo(http://www.tuicool.com/articles/miEfmm）

---
+++ src/demo/RecycleAdapter.java
@@ -0,0 +1,126 @@
+/**
+ * Date : 2014/7/15
+ * 
+ * @author edsheng
+ * 
+ */
+public class RecycleAdapter extends RecyclerView.Adapter<ViewHolder> {
+
+  private List<Bean> beans;
+
+  public RecycleAdapter(List<Bean> beans) {
+    super();
+    this.beans = beans;
+  }
+
+  /**
+   * 内部TextHoler
+   * 
+   * @author edsheng
+   * 
+   */
+  public class TextHoler extends RecyclerView.ViewHolder {
+    public TextView textView;
+
+    public TextHoler(View textview) {
+      super(textview);
+      this.textView = (TextView) textview.findViewById(R.id.mytext);
+    }
+  }
+
+  /**
+   * iamgeHolder
+   * 
+   * @author edsheng
+   * 
+   */
+  public class ImageHoler extends RecyclerView.ViewHolder {
+    public ImageView Imageview;
+
+    public ImageHoler(View textview) {
+      super(textview);
+      this.Imageview = (ImageView) textview.findViewById(R.id.myiamge);
+    }
+  }
+
+  /**
+   * 按钮的holder
+   * 
+   * @author edsheng
+   * 
+   */
+  public class ButtonHolder extends RecyclerView.ViewHolder {
+    public Button button;
+
+    public ButtonHolder(View textview) {
+      super(textview);
+      this.button = (Button) textview.findViewById(R.id.mybutton);
+    }
+  }
+
+  @Override
+  public int getItemCount() {
+    // TODO Auto-generated method stub
+    return beans.size();
+  }
+
+  /**
+   * 获取消息的类型
+   */
+  @Override
+  public int getItemViewType(int position) {
+    // TODO Auto-generated method stub
+    return beans.get(position).getType();
+  }
+
+  /**
+   * 创建VIewHolder
+   */
+  @Override
+  public ViewHolder onCreateViewHolder(ViewGroup parent, int viewtype) {
+    // TODO Auto-generated method stub
+    View v = null;
+    ViewHolder holer = null;
+    switch (viewtype) {
+    case Bean.X_TYPE:
+      v = LayoutInflater.from(parent.getContext()).inflate(
+          R.layout.recylce_item_x, null);
+      holer = new TextHoler(v);
+      break;
+    case Bean.Y_TYPE:
+      v = LayoutInflater.from(parent.getContext()).inflate(
+          R.layout.recylce_item_y, null);
+      holer = new ButtonHolder(v);
+      break;
+    case Bean.Z_TYPE:
+      v = LayoutInflater.from(parent.getContext()).inflate(
+          R.layout.recylce_item_z, null);
+      holer = new ImageHoler(v);
+      break;
+    }
+
+    return holer;
+  }
+
+  /**
+   * 绑定viewholder
+   */
+  @Override
+  public void onBindViewHolder(ViewHolder holder, int position) {
+    // TODO Auto-generated method stub
+    switch (getItemViewType(position)) {
+    case Bean.X_TYPE:
+      TextHoler textholer = (TextHoler) holder;
+      textholer.textView.setText(beans.get(position).getText());
+      break;
+    case Bean.Y_TYPE:
+      ButtonHolder buttonHolder = (ButtonHolder) holder;
+      buttonHolder.button.setText(beans.get(position).getText());
+      break;
+    case Bean.Z_TYPE:
+      ImageHoler imageHoler = (ImageHoler) holder;
+      // imageHoler.Imageview.setImageResource(android.R.drawable.checkbox_on_background);
+      break;
+    }
+  }
+}
